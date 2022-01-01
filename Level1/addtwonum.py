c056f76ca8201311947367d1c8cb3a3db0e1ddf6
채희찬 <eqholic1125@gmail.com>
2022-01-01 12:11:17 +0900

    refactor: add two num 중복 제거 및 정렬 단순화

--- Level1/addtwonum.py
+++ Level1/addtwonum.py
@@ -2,7 +2,5 @@ def solution(numbers):
     answer = []
     for first in range(len(numbers)):
         for second in range(first):
-            if((numbers[first] + numbers[second]) not in answer):
-                answer.append(numbers[first] + numbers[second])
-        answer.sort()
-    return answer
+            answer.append(numbers[first] + numbers[second])
+    return sorted(list(set(answer)))

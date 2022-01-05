e1081d75a04cb391efc6784a11a179121dfadf94
채희찬 <eqholic1125@gmail.com>
2022-01-05 22:17:57 +0900

    refactor: pokemon 라인 줄이기

--- Level1/pokemon.py
+++ Level1/pokemon.py
@@ -1,12 +1,2 @@
-import collections
-
-
 def solution(nums):
-    dic = collections.Counter(nums)
-    num_len = len(nums)/2
-    dic_len = len(dic)
-
-    if dic_len < num_len:
-        return dic_len
-    else:
-        return num_len
+    return min(len(set(nums)), len(nums)/2)

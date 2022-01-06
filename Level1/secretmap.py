e8697954bab8bc32cf7801f9adbeb49d60285011
Chae hee chan <eqholic1125@gmail.com>
2022-01-06 14:27:32 +0900

    refactor: secretmap 비트계산으로 변경

--- Level1/secretmap.py
+++ Level1/secretmap.py
@@ -1,15 +1,5 @@
 def solution(n, arr1, arr2):
-    answer = ['' for i in range(n)]
-    line = ""
-    for a,b,i in zip(arr1,arr2,range(n)):
-        arr1[i] = format(a, 'b').zfill(n).replace('1', '#').replace('0', ' ')
-        arr2[i] = format(b, 'b').zfill(n).replace('1', '#').replace('0', ' ')
-        for j in range(n):
-            if arr1[i][j] == '#' or arr2[i][j] == '#':
-              line += "#"
-            else: 
-              line += " "
-        answer.append(line)
+    answer = []
+    for a,b in zip(arr1,arr2):
+        answer.append(str(bin(a|b)[2:].zfill(n)).replace('1', '#').replace('0', ' '))
     return answer
-
-solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])

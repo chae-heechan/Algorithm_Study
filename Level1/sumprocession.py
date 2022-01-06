c4e5fdd50dd48ab830dc0a9e1c37897c8d51cbdd
Chae hee chan <eqholic1125@gmail.com>
2022-01-06 15:02:12 +0900

    refactor: sumprocession 반복문 단축

--- Level1/sumprocession.py
+++ Level1/sumprocession.py
@@ -1,10 +1,2 @@
 def solution(arr1, arr2):
-    answer = []
-    for i in range(len(arr1)) :
-        a = []
-        for j in range(len(arr1[0])):
-            a.append(arr1[i][j]+arr2[i][j])
-        answer.append(a)
-    
-    print(answer)
-    return answer
+    return [[c + d for c,d in zip(a,b)] for a,b in zip(arr1,arr2)] 

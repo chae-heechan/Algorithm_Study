633a69f1fb79a6bba0f6099206b1054b380b781c
Chae hee chan <eqholic1125@gmail.com>
2022-01-07 18:02:08 +0900

    refactor: collatz 분기문 단순화

--- Level1/collatz.py
+++ Level1/collatz.py
@@ -1,10 +1,7 @@
 def solution(num):
     answer = 0
     while num != 1 and answer < 500 :
-        if num % 2 == 0:    # 짝수일 경우
-            num /= 2
-        else:               # 홀수일 경우
-            num = (num*3) + 1 
+        num = (num/2) if num%2==0 else (num*3+1)
         answer += 1
         
     if answer >= 500:

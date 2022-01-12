3aaaabe6c867e000a5b2577ae6204cec8b25ca6e
Chae hee chan <eqholic1125@gmail.com>
2022-01-12 17:49:00 +0900

    feat: find prime number 구현

---
+++ Level1/find_prime_number.py
@@ -0,0 +1,11 @@
+def solution(n):
+    
+    answer = [True]*(n+1)
+    
+    m = int(n ** 0.5)
+    for i in range(2, m+1):
+        if answer[i] == True:
+            for j in range(i*i, n+1, i):
+                answer[j] = False
+            
+    return answer.count(True)-2

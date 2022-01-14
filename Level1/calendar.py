fd87e08251b77d0530a5380c6f54e80b0ba7745b
Chae hee chan <eqholic1125@gmail.com>
2022-01-14 13:55:20 +0900

    refactor: calendar 구조 변경

--- Level1/calendar.py
+++ Level1/calendar.py
@@ -1,9 +1,5 @@
 def solution(a, b):
-    cal = {1 : 31, 2 : 29, 3 : 31, 4 : 30, 5 : 31, 6 : 30, 7 : 31, 8 : 31, 9 : 30 , 10 : 31, 
-           11 : 30, 12 : 31 }
-    day = {0 : "THU", 1 : "FRI", 2 : "SAT", 3 : "SUN", 4 : "MON", 5 : "TUE", 6 : "WED"}
-    tmp = 0
-    for i in range(1,a) :
-        tmp += cal[i]
-    tmp = (tmp + b) % 7
-    return day[tmp]
+    cal = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    
+    return day[(sum(cal[:a-1])+b-1) % 7]

ce65cda1ed45d53df1e9b7f8e3db5756e9186476
Chae hee chan <eqholic1125@gmail.com>
2022-05-05 13:56:17 +0900

    descende intiger 다시풀기

--- Level1/descende_intiger.py
+++ Level1/descende_intiger.py
@@ -1,7 +1,2 @@
 def solution(n):
-    s = sorted([int(i) for i in str(n)])[::-1]
-    answer = ""
-    for i in s:
-        answer += str(i)
-    
-    return int(answer)
+    return int("".join(sorted([i for i in str(n)], reverse = True)))

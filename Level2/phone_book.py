053d449bd02c2ef039fd4c7d304f957ecb1604f2
Chae hee chan <eqholic1125@gmail.com>
2022-05-09 11:48:19 +0900

    phone book 풀기

---
+++ Level2/phone_book.py
@@ -0,0 +1,8 @@
+def solution(phone_book):
+
+    phone_book.sort()
+
+    for index in range(len(phone_book)-1):
+        if phone_book[index] == phone_book[index+1][:len(phone_book[index])]:
+            return False
+    return True

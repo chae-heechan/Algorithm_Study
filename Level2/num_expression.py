c61af0b1b97d790e2332581eb0b7da42c3e510fa
Chae hee chan <eqholic1125@gmail.com>
2022-05-24 22:27:57 +0900

    num expression 다시풀기

--- Level2/num_expression.py
+++ Level2/num_expression.py
@@ -1,15 +1,18 @@
 def solution(n):
     count = 0
+    
     for i in range(1, n+1):
         result = 0
         addNum = i
+        
         while(True):
             result += addNum
             addNum += 1
+            
             if result == n:
                 count += 1
                 break
             elif result > n:
                 break
-
+            
     return count

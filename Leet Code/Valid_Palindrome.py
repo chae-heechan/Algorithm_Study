2d73f528c9500eedd98f5ddd53badf448d5ab6af
채희찬 <eqholic1125@gmail.com>
2022-08-08 23:53:23 +0900

    refactor: valid palindrome 다시풀기

--- Leet Code/Valid_Palindrome.py
+++ Leet Code/Valid_Palindrome.py
@@ -1,16 +1,7 @@
 import re
 
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        palindrome_sentence = ""
         s = s.upper()
-
-        for i in s:
-            if re.match('[A-Z0-9]', i):
-                palindrome_sentence += i
-
-        if (palindrome_sentence[::-1]) == palindrome_sentence:
-            return True
-        else:
-            return False
+        s = re.sub('[^A-Z0-9]', '', s)
+        return s == s[::-1]

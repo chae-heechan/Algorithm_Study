91235cf3c3ef733fc5839c500cf9b1be662061f4
Chae hee chan <eqholic1125@gmail.com>
2022-08-13 12:12:18 +0900

    feat: reorder data in log files 풀기

---
+++ Leet Code/Reorder_Data_In_Log_Files.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():    # 숫자일 경우
+                digits.append(log)
+            else:   # 영어일 경우
+                letters.append(log)
+                
+        letters.sort(key = lambda x : (x.split()[1:], x.split()[0]))
+        return letters + digits

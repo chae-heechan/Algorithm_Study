2a1f60a60dd3422bb0efbbde47e64e86a83e5c66
채희찬 <eqholic1125@gmail.com>
2022-09-16 15:09:12 +0900

    feat: product of array except self 풀기

---
+++ Leet Code/product_of_array_except_self.py
@@ -0,0 +1,14 @@
+import numpy
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = numpy.ones(len(nums))
+        answer = answer.astype(int)
+
+        for i, v in enumerate(nums):
+            temp = answer[i]
+            answer = answer * v
+            answer[i] = temp
+
+        return list(answer)

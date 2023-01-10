e9672c6fb56242a4e0a895b71fdd2be09d8e398b
채희찬 <eqholic1125@gmail.com>
2023-01-10 23:03:50 +0900

    feat: discriminateSquareRoot.java 풀기

---
+++ Java/BeakJoon/discriminateSquareRoot.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        Double x = Math.sqrt(n);
+        if (x == x.intValue()) {
+            return (long) Math.pow(x + 1, 2);
+        } else {
+            return -1;
+        }
+    }
+}

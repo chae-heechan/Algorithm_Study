36df450cb0100c869846085df2403dcffca27ea8
채희찬 <eqholic1125@gmail.com>
2023-01-12 21:12:44 +0900

    feat: reverseSort.java 풀기

---
+++ Java/BeakJoon/reverseSort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class Solution {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        int len = s.length();
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            answer[i] = s.charAt(len - i - 1) - 48;
+        }
+        return answer;
+    }
+}

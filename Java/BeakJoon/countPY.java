8b732a0bf6d7c6b8adb0170691b55111b5e57919
채희찬 <eqholic1125@gmail.com>
2023-01-12 21:12:02 +0900

    feat: countPY.java 다시풀기

--- Java/BeakJoon/countPY.java
+++ Java/BeakJoon/countPY.java
@@ -1,15 +1,7 @@
 class Solution {
     boolean solution(String s) {
         s = s.toUpperCase();
-        int p = 0;
-        int y = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'P') {
-                p++;
-            } else if (s.charAt(i) == 'Y') {
-                y++;
-            }
-        }
-        return (p == y);
+        
+        return s.chars().filter(e -> 'P' == e).count() == s.chars().filter(e -> 'Y' == e).count();
     }
 }

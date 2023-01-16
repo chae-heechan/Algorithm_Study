57ed54f1c237c7a6564018ada0e26bde37b11a4e
채희찬 <eqholic1125@gmail.com>
2023-01-16 21:34:55 +0900

    feat: harshadNumber.java 풀기

---
+++ Java/Programmers/harshadNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        String str = Integer.toString(x);
+        for (int i = 0; i < str.length(); i++) {
+            sum += str.charAt(i) - 48;
+        }
+
+        return x % sum == 0;
+    }
+}

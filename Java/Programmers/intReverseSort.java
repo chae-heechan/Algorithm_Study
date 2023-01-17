72845d79380057c2df78747368b323ebe938ea3e
채희찬 <eqholic1125@gmail.com>
2023-01-17 22:45:19 +0900

    feat: intReverseSort.java 풀기

---
+++ Java/Programmers/intReverseSort.java
@@ -0,0 +1,22 @@
+import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+
+        String s = Long.toString(n);
+        Integer[] ia = Stream.of(s.split("")).map(Integer::parseInt).toArray(Integer[]::new);
+
+        Arrays.sort(ia);
+
+        Arrays.sort(ia, Collections.reverseOrder());
+
+        String result = Arrays.toString(ia).replaceAll("[^0-9]", "");
+
+        answer = Long.valueOf(result);
+
+        return answer;
+    }
+}

2acfef7ed04d4345aa5122d78eda147fb3537173
채희찬 <eqholic1125@gmail.com>
2023-01-18 20:26:26 +0900

    feat: Remainder1.java 풀기

---
+++ Java/Programmers/Remainder1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer;
+
+        for (answer = 1; answer < n; answer++) {
+            if (n % answer == 1) {
+                return answer;
+            }
+        }
+
+        return answer;
+    }
+}

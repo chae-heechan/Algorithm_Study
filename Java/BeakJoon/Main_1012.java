65255df992b162a72284ed6a185812577350cfbf
huichan <eqholic1125@gmail.com>
2023-02-06 17:44:37 +0900

    refactor: 백준 1012 코드 수정

--- Java/BeakJoon/Main_1012.java
+++ Java/BeakJoon/Main_1012.java
@@ -5,7 +5,7 @@ public class Main_1012 {
     static int Y;
     static int X;
     static int[][] datos;
-    static int[][] visited;
+    static int[][] visite;
 
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
@@ -21,8 +21,9 @@ public class Main_1012 {
             int cuentaDeCol = sc.nextInt();
 
             datos = new int[X][Y];
-            visited = new int[X][Y];
+            visite = new int[X][Y];
 
+            // datos로 데이터 입력
             for (int i = 0; i < cuentaDeCol; i++) {
                 int y = sc.nextInt();
                 int x = sc.nextInt();
@@ -30,25 +31,26 @@ public class Main_1012 {
                 datos[x][y] = 1;
             }
 
+            // 이동하다가 datos가 1이고 visite가 0일 경우 dfs;
             for (int i = 0; i < X; i++) {
                 for (int j = 0; j < Y; j++){
-                    if (datos[i][j] == 1 && visited[i][j] == 0) {
+                    if (datos[i][j] == 1 && visite[i][j] == 0) {
                         cuenta++;
                         dfs(i, j);
                     }
                 }
             }
-            // 이동하다가 1만나면 dfs
 
             System.out.println(cuenta);
         }
     }
 
+    // 방문 찍고 4방탐색 후 조건 맞으면 dfs호출
     public static void dfs(int x, int y){
-        visited[x][y] = 1;
+        visite[x][y] = 1;
         for(int i = 0; i < 4; i++) {
             if (x+dx[i] >= 0 && x+dx[i] < X && y+dy[i] >= 0 && y+dy[i]< Y) {
-                if (datos[x + dx[i]][y + dy[i]] == 1 && visited[x+dx[i]][y+dy[i]] == 0) {
+                if (datos[x + dx[i]][y + dy[i]] == 1 && visite[x+dx[i]][y+dy[i]] == 0) {
                     dfs(x+dx[i], y+dy[i]);
                 }
             }

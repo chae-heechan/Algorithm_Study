e13ec2d8a6da0c29ab45a3386019e6b16eeb497b
huichan <eqholic1125@gmail.com>
2023-02-07 15:33:37 +0900

    feat: SWEA 2805 풀기

---
+++ Java/SWExpertAcademy/Solution_2805.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution_2805 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int talla;
+    static int[][] hacienda;
+
+    public static void main(String[] args) throws IOException {
+
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= tc; t++){
+            int ganancia = 0;
+            int alcance = 1;
+
+            talla = Integer.parseInt(br.readLine());
+
+            hacienda = new int[talla][talla];
+
+            for (int i = 0; i < talla; i++) {
+                hacienda[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+            }
+
+            int comienzo = talla/2;
+
+            for (int i = 0; i < talla/2; i++){
+                for (int j = comienzo; j < comienzo +alcance; j++){
+                    ganancia += hacienda[i][j];
+                }
+                comienzo -= 1;
+                alcance += 2;
+            }
+            for (int i = talla/2; i < talla; i++){
+                for (int j = comienzo; j < comienzo +alcance; j++){
+                    ganancia += hacienda[i][j];
+                }
+                comienzo += 1;
+                alcance -= 2;
+            }
+
+            System.out.printf("#%d %d\n", t, ganancia);
+
+
+        }
+    }
+}

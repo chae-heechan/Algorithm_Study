d50cc32b3120340be4dd311ec3b7ef6cf8a1c66d
huichan <eqholic1125@gmail.com>
2023-02-08 15:55:57 +0900

    feat: 백준 11659 풀기

--- Java/BeakJoon/Main_10163.java
+++ Java/BeakJoon/Main_10163.java
@@ -1,2 +1,47 @@
-package BeakJoon;public class Main_10163 {
+package BeakJoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_10163 {
+    private static final int SIZE = 1001;
+    public static void main(String[] args) throws IOException {
+        int[] result = new int[SIZE];
+        int[][] arr = new int[SIZE][SIZE];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int count = (int)(str.charAt(0)) - '0';
+
+        for (int c = 1; c <= count; c++) {
+            int[] line = Arrays.stream((br.readLine().split(" "))).mapToInt(Integer::parseInt).toArray();
+            int x = line[0];
+            int y = line[1];
+            int a = line[3]+y;
+            int b = line[2]+x;
+
+            for (int i = x; i < a; i++){
+                for (int j = y; j < b; j++) {
+                    arr[i][j] = c;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++) {
+                if (arr[i][j] != 0) {
+                    result[arr[i][j]]++;
+                }
+            }
+        }
+
+        for(int i = 1; i <= count; i++) {
+            System.out.println(result[i]);
+        }
+
+    }
 }
---
+++ Java/BeakJoon/Main_11659.java
@@ -0,0 +1,31 @@
+package BeakJoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int n = nm[0];
+        int m = nm[1];
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] sums = new int[n+1];
+
+        for(int i = 1; i <= n; i++){
+            sums[i] = sums[i-1] + arr[i-1];
+        }
+
+        for (int i = 0; i < m; i++) {
+            int[] ab = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            sb.append(sums[ab[1]] - sums[ab[0]-1]);
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}

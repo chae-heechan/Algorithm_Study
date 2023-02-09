0a0be169b60859085170ae1ec5571be122d36f4b
huichan <eqholic1125@gmail.com>
2023-02-09 17:04:36 +0900

    feat: 백준 2961 풀기

---
+++ Java/BeakJoon/Main_2961.java
@@ -0,0 +1,39 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2961 {
+    static int n;
+    static long min;
+    static int[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        min = Long.MAX_VALUE;
+
+        dfs(0, 1, 0);
+
+        System.out.println(min);
+    }
+
+    public static void dfs(int v, int mul, int add){
+        if (v == n){
+            if(add != 0 && min > Math.abs(mul - add)){
+                min = Math.abs(mul - add);
+            }
+            return;
+        }
+
+        dfs(v+1, mul * arr[v][0], add + arr[v][1]);
+        dfs(v+1, mul, add);
+    }
+}

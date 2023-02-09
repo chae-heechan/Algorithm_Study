9fcb12898e716ac30a32e26ba94ca245c40a899c
huichan <eqholic1125@gmail.com>
2023-02-09 17:54:35 +0900

    feat: SWEA 2001 풀기

---
+++ Java/SWExpertAcademy/Solution_2001.java
@@ -0,0 +1,43 @@
+package SWExpertAcademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution_2001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int TC = Integer.parseInt(br.readLine());
+
+        for(int t = 1; t <= TC; t++) {
+            int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            int n = nm[0];
+            int m = nm[1];
+            int max = 0;
+
+            int[][] arr = new int[n + 1][n + 1];
+            int[][] sum = new int[n + 1][n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                for (int j = 1; j <= n; j++) {
+                    sum[i][j] = sum[i][j - 1] + sum[i - 1][j] + arr[i][j] - sum[i - 1][j - 1];
+                }
+            }
+
+            for (int i = 1; i <= n - m + 1; i++) {
+                for (int j = 1; j <= n - m + 1; j++) {
+                    int fly = sum[i+m-1][j+m-1] - sum[i-1][j+m-1] - sum[i+m-1][j-1] + sum[i-1][j-1];
+                    max = Math.max(max, fly);
+                }
+
+            }
+
+            System.out.printf("#%d %d\n", t, max);
+        }
+
+    }
+}

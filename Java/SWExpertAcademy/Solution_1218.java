d80e7ae0d3c66778a3f416a4f493574f789d43db
huichan <eqholic1125@gmail.com>
2023-02-10 12:22:06 +0900

    feat: SWEA 1218 풀기

---
+++ Java/SWExpertAcademy/Solution_1218.java
@@ -0,0 +1,53 @@
+package SWExpertAcademy;
+
+import com.sun.jmx.remote.internal.ArrayQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution_1218 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int t = 1; t <= 10; t++) {
+
+            int answer = 1;
+
+            int n = Integer.parseInt(br.readLine());
+
+            char[] brackets = br.readLine().toCharArray();
+
+            Stack<Character> a = new Stack<>();
+
+            for (char bracket : brackets) {
+                if (a.isEmpty() || bracket == '(' || bracket == '{' || bracket == '[' || bracket == '<') {
+                    a.push(bracket);
+                    continue;
+                }
+
+                if (a.peek() == '(' && bracket == ')') {
+                    a.pop();
+                    continue;
+                } else if (a.peek() == '{' && bracket == '}') {
+                    a.pop();
+                    continue;
+                } else if (a.peek() == '[' && bracket == ']') {
+                    a.pop();
+                    continue;
+                } else if (a.peek() == '<' && bracket == '>') {
+                    a.pop();
+                    continue;
+                }
+
+                answer = 0;
+
+            }
+
+            System.out.printf("#%d %d\n", t, answer);
+        }
+
+
+    }
+}

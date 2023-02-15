c49f602dd6e3c5205446eca7912d1214258d1960
huichan <eqholic1125@gmail.com>
2023-02-15 15:32:13 +0900

    feat: 백준 2563 풀기

---
+++ Java/BeakJoon/Main_2563.java
@@ -0,0 +1,46 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 실버 5
+// 색종이
+public class Main_2563 {
+    private static final int SIZE = 1001;
+    public static void main(String[] args) throws IOException {
+        int result = 0;
+        int[][] arr = new int[SIZE][SIZE];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+
+        while (count-- > 0) {
+            int[] line = Arrays.stream((br.readLine().split(" "))).mapToInt(Integer::parseInt).toArray();
+            int x = line[0];
+            int y = line[1];
+            int a = x + 10;
+            int b = y + 10;
+
+            for (int i = x; i < a; i++){
+                for (int j = y; j < b; j++) {
+                    arr[i][j] = 1;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++) {
+                if (arr[i][j] == 1) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}

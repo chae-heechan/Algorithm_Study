dc41595fa02cc56aa878f0802de0a90fa353eb91
chaehuichan <eqholic1125@gmail.com>
2023-02-25 19:47:54 +0900

    feat: 백준 7893 풀기

--- Java/BeakJoon/countPY.java
+++
@@ -1,7 +0,0 @@
-//class Solution {
-//    boolean solution(String s) {
-//        s = s.toUpperCase();
-//
-//        return s.chars().filter(e -> 'P' == e).count() == s.chars().filter(e -> 'Y' == e).count();
-//    }
-//}

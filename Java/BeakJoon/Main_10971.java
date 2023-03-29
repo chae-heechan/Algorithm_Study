4b7bf9554a6a9034d66a7bc3ba12357df4886772
chaehuichan <eqholic1125@gmail.com>
2023-03-29 22:52:59 +0900

    feat: 백준 10971 풀기

---
+++ Java/BeakJoon/Main_10971.java
@@ -0,0 +1,57 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 실버 2
+// 외판원 순회 2
+public class Main_10971 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] map;
+    static boolean[] visited;
+    static int n;
+    static int minCost = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        visited = new boolean[n];
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            visited[i] = true;
+            dfs(i, i, 0, 0);
+            visited[i] = false;
+        }
+
+        if (minCost == Integer.MAX_VALUE)
+            System.out.println(0);
+        else
+            System.out.println(minCost);
+    }
+
+    private static void dfs(int start, int prev, int count, int sum) {
+        if (count == n - 1) {
+            if (map[prev][start] == 0) {
+                return;
+            }
+            minCost = Math.min(minCost, sum + map[prev][start]);
+            return;
+        }
+
+        if (minCost <= count) return;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == false && map[prev][i] != 0) {
+                visited[i] = true;
+                dfs(start, i, count+1, sum + map[prev][i]);
+                visited[i] = false;
+            }
+        }
+    }
+}

af9551f338a44724e799e8da543b02dfb208f409
chaehuichan <eqholic1125@gmail.com>
2023-05-02 23:49:10 +0900

    feat: 백준 11654 풀기

---
+++ Java/BeakJoon/Main_1114.java
@@ -0,0 +1,22 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 통나무 자르기
+public class Main_1114 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l, k, c;
+
+        int[] lkc = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        l = lkc[0];
+        k = lkc[1];
+        c = lkc[2];
+
+
+
+    }
+}
---
+++ Java/BeakJoon/Main_11654.java
@@ -0,0 +1,15 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 브론즈 5
+// 아스키 코드
+public class Main_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println((int)(br.readLine().charAt(0)));
+    }
+}

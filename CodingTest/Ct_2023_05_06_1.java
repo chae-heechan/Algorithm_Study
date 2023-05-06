ba2f8acd46f3751058f9c096b6167bb5167831ab
chaehuichan <eqholic1125@gmail.com>
2023-05-06 13:31:09 +0900

    feat: 코테 풀기

---
+++ CodingTest/Ct_2023_05_06_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Ct_2023_05_06_1 {
+    public static void main(String[] args) {
+        int[] boxes = {1, 2};
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append('c');
+
+        sb.toString();
+
+        System.out.println(sb);
+
+
+    }
+}

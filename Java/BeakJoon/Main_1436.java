f29ef786997ae699e097bfe1d6110d7aac52012f
Chae Huichan <eqholic1125@gmail.com>
2023-08-22 20:19:20 +0900

    feat: 백준 1436 풀기

---
+++ Java/BeakJoon/Main_1436.java
@@ -0,0 +1,23 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        int num = 665;
+
+        while (count < n) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+
+        System.out.println(num);
+    }
+}

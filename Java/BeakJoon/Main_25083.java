5b172d4c860fa4ca3514c3c4d48b34df3196f232
chaehuichan <eqholic1125@gmail.com>
2024-05-25 14:10:48 +0900

    feat: 백준 25083 풀기

---
+++ Java/BeakJoon/Main_25083.java
@@ -0,0 +1,15 @@
+package BeakJoon;
+
+public class Main_25083 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n" +
+            "r`-_   ,'  ,/\n" +
+            " \\. \". L_r'\n" +
+            "   `~\\/\n" +
+            "      |\n" +
+            "      |");
+
+        System.out.println(sb);
+    }
+}

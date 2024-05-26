bc33ff48c0d60bf80eeb25b919aacf0d80ca9d03
chaehuichan <eqholic1125@gmail.com>
2024-05-26 17:34:23 +0900

    feat: 백준 2475 풀기

---
+++ Java/BeakJoon/Main_2475.java
@@ -0,0 +1,21 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number * number;
+        }
+        System.out.println(sum % 10);
+    }
+}

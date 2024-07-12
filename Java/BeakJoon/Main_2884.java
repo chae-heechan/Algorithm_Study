6300cba818f7fecbc9059f831475ead1e0d8ba36
chae-heechan <eqholic1125@gmail.com>
2024-07-12 23:24:20 +0900

    feat: 백준 2884 풀기

---
+++ Java/BeakJoon/Main_2884.java
@@ -0,0 +1,29 @@
+package BeakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+
+        if (minute < 45) {
+            minute += 15;
+            if (hour > 0) {
+                hour--;
+            }else {
+                hour = 23;
+            }
+        }else {
+            minute -= 45;
+        }
+
+        System.out.println(hour + " " + minute);
+    }
+}
